3c0f36da734a37c923e209da0c77bbd26167b32f
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-01 10:25:16 -0600

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Studio.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+
+@studio = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+
+@movie_1 = @studio.movies.create!(title:'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure') 
+@movie_2 = @studio.movies.create!(title:'Jurassic Park', creation_year: 1993, genre: 'Action/Adventure') 
+@movie_3 = @studio.movies.create!(title:'Minions', creation_year: 2015, genre: 'Family Comedy') 
+@movie_4 = @studio.movies.create!(title:'Shrek', creation_year: 2001, genre: 'Family Comedy') 
+@movie_5 = @studio.movies.create!(title:'Back to the Future', creation_year: 1985, genre: 'Sci-Fi') 
+
+@actor_1 = @movie_1.actors.create!(name: 'Harrison Ford', age: 78, currently_working: false)
+@actor_2 = @movie_2.actors.create!(name: 'Laura Dern', age: 54, currently_working: true)
+@actor_3 = @movie_2.actors.create!(name: 'Samuel L. Jackson', age: 72, currently_working: true)
+@actor_4 = @movie_5.actors.create!(name: 'Christopher Lloyd', age: 82, currently_working: true)
+@actor_5 = @movie_5.actors.create!(name: 'Lea Thompson', age: 60, currently_working: true)
+@actor_6 = @movie_5.actors.create!(name: 'Michael J. Fox', age: 59, currently_working: false)
+@actor_7 = @movie_5.actors.create!(name: 'Claudia Wells', age: 54, currently_working: true)
+
+@movie_2.actors << @actor_4
