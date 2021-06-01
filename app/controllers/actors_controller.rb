8e8a28216673fd520e389ed57692cbb7afa025c1
Molly Krumholz <mkkrumholz@gmail.com>
2021-06-01 11:47:18 -0600

    Pull coactors data logic into actors model

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,9 +1,6 @@
 class ActorsController < ApplicationController
   def show
     @actor = Actor.find(params[:id])
-    @coactors =  Actor.find_by_sql("select distinct(name) from actors 
-                                    left join cast_members on cast_members.actor_id = actors.id 
-                                    where movie_id in (select movie_id from cast_members where actor_id = #{@actor.id})
-                                    and actors.id != #{@actor.id}")
+    @coactors = @actor.coactors
   end 
 end
